2bb202d62941ae7373fd6ecbc9e5a86b3a339b3c
Carolina <carolinarb1994@gmail.com>
2025-06-27 09:25:23 +0200

    finish project

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ def crear():
     db.session.commit() # Guardamos los cambios en la base de datos
     return redirect(url_for('home'))
 
+@app.route('/eliminar-tarea/<id>')
+def eliminar(id):
+    tarea = db.session.query(Tarea).filter_by(id=int(id)).delete()
+    db.session.commit() # Ejecutar la operación pendiente de la base de datos
+    return redirect(url_for('home'))
+
+@app.route('/tarea-hecha/<id>')
+def hecha(id):
+    tarea = db.session.query(Tarea).filter_by(id=int(id)).first() # Se obtiene la tarea que se busca
+    tarea.hecha = not(tarea.hecha) # Guardamos en la variable booleana de la tarea, su contrario
+    db.session.commit() # Ejecutar la operación pendiente de la base de datos
+    return redirect(url_for('home'))
 @app.route('/')
 def home():
     todas_las_tareas = db.session.query(Tarea).all()  # Obtenemos todas las tareas
